c618b01cbd011aa6a807b63a020b2321dd78dbe2
VithuJey <vithushjeytharma@gmail.com>
2020-02-04 19:47:00 +0530

    Add functions to ping the URL and scrape vegetable list

---
+++ app.js
@@ -0,0 +1,52 @@
+const puppeteer = require("puppeteer");
+
+// URL to be scraped
+let URL = "http://snapcart.lk/price-updates/all-vegetable-prices.php";
+
+// Open the above URL in a browser's new page
+ping = async () => {
+  const browser = await puppeteer.launch({ headless: true });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 926 });
+  await page.goto(URL);
+  return page;
+};
+
+// Evaluate & scrape
+let itemsDetails;
+ping()
+  .then(page => {
+    itemsDetails = page.evaluate(() => {
+      let items = [];
+      let itemJson = {};
+
+      let items = document.querySelectorAll("div[class='col-sm-6']");
+
+      for (let item of items) {
+        try {
+          itemJson.name = item.querySelector(
+            "div[class='col-sm-6'] > a > div > div[class='price-cont veg-content'] > div > h3"
+          );
+          if ("vegetable" in URL) itemJson.type = "vegetable";
+          else itemJson.type = "fruit";
+          itemJson.weight = "1 kg";
+          itemJson.details = [
+            {
+              date: new Date().getDate(),
+              price: item.querySelector(
+                "div[class='col-sm-6'] > a > div > div[class='price-cont veg-content'] > div > span[class='price']"
+              )
+            }
+          ];
+        } catch (error) {
+          console.log(error);
+        }
+
+        items.push(itemJson);
+      }
+
+      return itemJson;
+    });
+    console.log(itemsDetails);
+  })
+  .catch(error => console.log(error));
--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # Scraper for www.snapcart.lk
 
-A Puppeteer scraping script to scrape vegetables and fruits details from www.snapcart.lk and save it to MongoDB Atlas.
+A Puppeteer scraping script to scrape vegetables' and fruits' supermarket pricing details daily from www.snapcart.lk and save it to MongoDB Atlas.
